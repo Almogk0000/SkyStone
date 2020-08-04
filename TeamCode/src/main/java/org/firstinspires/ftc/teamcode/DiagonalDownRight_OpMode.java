5e73990965bc4c3bb11ad6d01bef435679390adf
EliorYousefi <elior@abilityedge.co.il>
2020-08-04 21:56:32 +0300

    Diagonal Back Bug Fixes.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DiagonalDownRight_OpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DiagonalDownRight_OpMode.java
@@ -47,7 +47,7 @@ public class DiagonalDownRight_OpMode extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftMotor_b = null;
     private DcMotor rightMotor_u = null;
-    private double power = -.3;
+    private double power = -0.3;
     private long runFor = 3000;
 
     @Override
